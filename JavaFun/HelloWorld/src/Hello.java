94767c2d64a0d57e6f58e12b3a8082b64dde7355
KyleJulianGreen <50314541+KyleJulianGreen@users.noreply.github.com>
2019-06-14 20:04:52 -0500

    Add files via upload
    Kyle_Green_First_Java_assignment

---
+++ JavaFun/HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args){
+
+        System.out.println("My name is Kyle Julian");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Chicago, Illinois");
+
+    }
+
+
+}
